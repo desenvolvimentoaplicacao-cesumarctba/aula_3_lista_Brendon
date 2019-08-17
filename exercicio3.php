a24b645973f2bbfd3a77d44e0f7f130bd6c11586
brendonrgb <53630100+brendonrgb@users.noreply.github.com>
2019-08-16 22:18:50 -0300

    exercicio 3

---
+++ exercicio3.php
@@ -0,0 +1,10 @@
+<?php
+
+/*EX 3*/
+$fahrenheit = readline ("digite a temperatura em graus fahrenheit:");
+$celsius = ((($fahrenheit -32)* 5)/9);
+echo "a temperatura em celsius é:" . $celsius . "\n";
+
+
+
+?>
